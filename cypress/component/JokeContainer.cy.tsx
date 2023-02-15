702414d2a930dd8fa18e65650b12afdf8b6580b0
Chris <caston88@gmail.com>
2023-02-15 17:43:28 +0000

    Add initial passing test for JokeContainer

--- cypress/component/JokeContainer.cy.tsx
+++ cypress/component/JokeContainer.cy.tsx
@@ -3,5 +3,6 @@ import JokeContainer from '../../src/components/JokeContainer';
 describe('JokeContainer.cy.tsx', () => {
   it('should contain the joke element', () => {
     cy.mount(<JokeContainer />);
+    cy.getByData('joke-container').should('exist');
   });
 });
--- src/components/JokeContainer.tsx
+++ src/components/JokeContainer.tsx
@@ -1,3 +1,3 @@
-export default function JokeContainer() {
+export default function Joke() {
   return <div className="joke-container" data-test="joke-container"></div>;
 }
