becd4424338776bce373678c716299db5e402370
Chris <caston88@gmail.com>
2023-02-18 17:25:10 +0000

    Add heading and background colour

--- src/components/HomeContainer/HomeContainer.tsx
+++ src/components/HomeContainer/HomeContainer.tsx
@@ -4,6 +4,9 @@ import './HomeContainer.css';
 const HomeContainer = () => {
   return (
     <div className="home-container" data-test="home-container">
+      <div>
+        <h1>CLICK FOR A DAD JOKE</h1>
+      </div>
       <JokeCardContainer />
     </div>
   );
