b9715c1223524616375f6e0725c3f722cb714107
Chris <caston88@gmail.com>
2023-02-18 17:05:50 +0000

    Add initial e2e test for button click behaviour

--- cypress/e2e/App.cy.ts
+++ cypress/e2e/App.cy.ts
@@ -7,4 +7,20 @@ describe('visits main page', () => {
     cy.getByData('home-container').should('exist');
     cy.getByData('joke-card-container').should('exist');
   });
+  it('should display a new joke when button is pressed', () => {
+    cy.intercept('GET', 'https://icanhazdadjoke.com/').as('getRandomJoke');
+    cy.visit('http://localhost:3000/');
+
+    cy.wait('@getRandomJoke').then((interception: any) => {
+      assert.isNotNull(interception.response.id, 'Initial API call has id');
+    });
+
+    cy.get('@getRandomJoke.all').should('have.length', 1);
+
+    cy.getByData('joke-container-button').click();
+
+    cy.get('@getRandomJoke.all').should('have.length', 2);
+  });
 });
+
+// cy.wait('@getRandomJoke').then(console.log)
